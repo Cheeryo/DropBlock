d904a7bdbb41e07057b75c4eba051b04d0193458
Nicolas Fischer <nicolas.fischer@hs-augsburg.de>
2017-03-23 01:57:55 +0100

    Hinzufügen Test PlayerController, 2. Scene zum Testen von weiterem PlayerController + Animator

---
+++ Block Dodge/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public bool isGrounded;
+    public bool isJumping;
+
+    public Rigidbody rb;
+
+    public float sideSpeed;
+    public float moveSpeed;
+    public float jumpForce;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        sideSpeed = Input.GetAxis("Horizontal") * moveSpeed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                isJumping = true;
+                jumpForce = 200;
+            }
+        }
+        Debug.Log(isGrounded);
+	}
+
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(sideSpeed,jumpForce,0));
+    } 
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("You hit it. " + other.collider.name);
+        if (other.collider.tag == "Floor")
+        {
+            isGrounded = true;
+            isJumping = false;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.collider.tag == "Floor")
+        {
+            isGrounded = false;
+            jumpForce = 0;
+        }
+    }
+}
