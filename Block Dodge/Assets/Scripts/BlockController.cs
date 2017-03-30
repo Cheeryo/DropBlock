44c9a1ee4af2af8b05b191cd747517d1ea169382
Nicolas Fischer <nicolas.fischer@hs-augsburg.de>
2017-03-30 22:40:42 +0200

    Added randomized BlockSpawning and randomized BlockFallSpeed

--- Block Dodge/Assets/Scripts/BlockController.cs
+++ Block Dodge/Assets/Scripts/BlockController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BlockController : MonoBehaviour {
 
-    private float blockFallSpeed = -5.0f;
+    private float blockFallSpeed;
     Vector3 finalSpeed;
     private bool negateMoving;
     private Vector3 locked;
@@ -12,18 +12,49 @@ public class BlockController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         negateMoving = false;
+        declareFallSpeed();
+        Debug.Log(blockFallSpeed);
         finalSpeed = new Vector3(0, blockFallSpeed, 0);
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        Debug.Log(negateMoving);
         transform.Translate(finalSpeed * Time.deltaTime);
         if (negateMoving)
         {
             transform.position = locked;
         }
+    }
+
+    private float declareFallSpeed()
+    {
+        int declareNumber = (int) Random.Range(1.0f, 26.0f);
+        if (declareNumber <= 4)
+        {
+            blockFallSpeed = Random.Range(-15.0f, -1.0f);
+        }
+        else if (declareNumber == 5 || declareNumber == 6)
+        {
+            blockFallSpeed = -1.0f;
+        }
+        else if (declareNumber > 6 && declareNumber < 12)
+        {
+            blockFallSpeed = -3.0f;
+        }
+        else if (declareNumber > 11 && declareNumber < 19)
+        {
+            blockFallSpeed = -5.0f;
+        }
+        else if (declareNumber > 18 && declareNumber < 24)
+        {
+            blockFallSpeed = -10.0f;
+        }
+        else if (declareNumber == 24 || declareNumber == 25)
+        {
+            blockFallSpeed = -15.0f;
+        }
+        return blockFallSpeed;
     }
 
     private void OnCollisionEnter(Collision col)
--- Block Dodge/Assets/Scripts/BlockManager.cs
+++ Block Dodge/Assets/Scripts/BlockManager.cs
@@ -18,7 +18,7 @@ public class BlockManager : MonoBehaviour
 
     private int blockSelector()
     {
-        blockNumber = (int) Random.Range(-0.1f, 5.0f);
+        blockNumber = (int) Random.Range(1.0f, 21.0f);
         return blockNumber;
     }
 	
@@ -29,7 +29,30 @@ public class BlockManager : MonoBehaviour
         {
             blockSelector();
             currentInterval = 0;
-            GameObject.Instantiate(blockPrefab[0], new Vector3(Random.Range(-10, 10) + .5f, 27, 0), Quaternion.Euler(0,90,0), blockContainer);
+            if (blockNumber <= 8)
+            {
+                GameObject.Instantiate(blockPrefab[0], new Vector3(Random.Range(-10, 11) + 0.5f, 27, 0), Quaternion.Euler(0, 90, 0), blockContainer);
+            }
+            else if (blockNumber == 9 || blockNumber == 10)
+            {
+                GameObject.Instantiate(blockPrefab[1], new Vector3(Random.Range(-9, 10), 26, 0), Quaternion.Euler(0, 90, 0), blockContainer);
+            }
+            else if (blockNumber > 10 && blockNumber < 15)
+            {
+                GameObject.Instantiate(blockPrefab[2], new Vector3(Random.Range(-10, 11) + 0.5f, 26, 0), Quaternion.Euler(0, 90, 0), blockContainer);
+            }
+            else if (blockNumber == 15)
+            {
+                GameObject.Instantiate(blockPrefab[3], new Vector3(Random.Range(-10, 11) + 0.5f, 25, 0), Quaternion.Euler(0, 90, 0), blockContainer);
+            }
+            else if (blockNumber == 16 || blockNumber == 17)
+            {
+                GameObject.Instantiate(blockPrefab[4], new Vector3(Random.Range(-9, 10), 27, 0), Quaternion.Euler(0, 90, 0), blockContainer);
+            }
+            else if (blockNumber == 18)
+            {
+                GameObject.Instantiate(blockPrefab[5], new Vector3(Random.Range(-8, 9) + 0.5f, 27, 0), Quaternion.Euler(0, 90, 0), blockContainer);
+            }
         }
 	}
 }
