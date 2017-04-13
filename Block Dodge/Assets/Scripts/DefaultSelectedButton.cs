ee3316b77b52e0edec22cafc2ef407dcf1995541
Nicolas Fischer <nicolas.fischer@hs-augsburg.de>
2017-04-14 00:30:15 +0200

    Changes -Added missing materials

---
+++ Block Dodge/Assets/Scripts/DefaultSelectedButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DefaultSelectedButton : MonoBehaviour {
+
+    public EventSystem eventSystem;
+    public GameObject defaultButton;
+
+    public void OnOpen()
+    {
+        if (defaultButton != null)
+        {
+            eventSystem.SetSelectedGameObject(defaultButton);
+        }
+    }
+
+}
