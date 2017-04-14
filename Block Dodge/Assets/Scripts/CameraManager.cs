c5a194d756fdde6e8d115d4a3e24d0472172d18d
Max Schöberl <maximilian.schoeberl@hs-augsburg.de>
2017-04-15 01:42:57 +0200

    1.) Kamera zoomt und bewegt sich nun dynamisch.

--- Block Dodge/Assets/Scripts/CameraManager.cs
+++ Block Dodge/Assets/Scripts/CameraManager.cs
@@ -1,20 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private float lerpSpeed;
-    private Vector3 offset;
+    [SerializeField] private float zoomMultiplier;
+
+    private Vector3 startPosition;
+    private PlayerController[] players;
 
 	private void Start ()
     {
-        offset = Camera.main.transform.position - player.position;
+        startPosition = Camera.main.transform.position;
+        players = GameObject.FindObjectsOfType<PlayerController>();
 	}
 	
 	private void LateUpdate ()
     {
-        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, new Vector3(Camera.main.transform.position.x, player.position.y + offset.y, Camera.main.transform.position.z), Time.deltaTime * lerpSpeed);
+        Bounds b = CalculateBounds();
+        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, new Vector3(b.center.x, Mathf.Clamp(3.5f-b.size.y,0,3.5f) + b.center.y, startPosition.z - Mathf.Abs(b.size.y) * zoomMultiplier), Time.deltaTime * lerpSpeed);
+    }
+
+    private Bounds CalculateBounds()
+    {
+        float minX, minY, maxX, maxY;
+
+        List<Vector3> positions = new List<Vector3>();
+        foreach(PlayerController p in players)
+        {
+            positions.Add(p.transform.position);
+        }
+
+        positions = positions.OrderBy(o => o.x).ToList();
+        maxX = positions[0].x;
+        minX = positions[positions.Count - 1].x;
+        positions = positions.OrderBy(o => o.y).ToList();
+
+        maxY = positions[0].y;
+        minY = positions[positions.Count - 1].y;
+
+        return new Bounds(new Vector3((minX+maxX)*.5f,(minY + maxY) * .5f, 0), new Vector3(minX - maxX, minY - maxY, 0));
     }
 }
