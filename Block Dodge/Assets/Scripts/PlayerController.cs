5780ef511d166dc72ed60214607acbf6c7117f92
Max Schöberl <maximilian.schoeberl@hs-augsburg.de>
2017-04-14 21:35:15 +0200

    1.) Collision-Funktion des Spielers wird nun richtig ausgeführt

--- Block Dodge/Assets/Scripts/PlayerController.cs
+++ Block Dodge/Assets/Scripts/PlayerController.cs
@@ -196,7 +196,7 @@ public class PlayerController : MonoBehaviour {
             # the collision point was within the upper half of the player -> cube above player
             # the cube is currently falling
         */
-        if (col.collider.CompareTag("Block") && col.contacts[0].point.y > transform.position.y && (col.rigidbody.velocity.y > .1f || col.rigidbody.velocity.y < -.1f))
+        if (col.collider.CompareTag("Block") && col.contacts[0].point.y > transform.position.y && !col.collider.GetComponent<BlockController>().Locked)
         {
             Debug.Log("You were squeezed by a cube!");
             //Locks (freezes) the cube to prevent bugs in the editor -> will not be needed later due to players death on this event
